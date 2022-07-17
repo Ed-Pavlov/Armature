6af230e7459130b874817a670148602cc5b104a1
Ed-Pavlov <ed@pavlov.is>
2022-07-17 18:03:59 +0200

    Changed the behaviour of TreatAllTuner.GetOrAddNodeTo. Now it uses AnyUnit() pattern in any case.

--- tests/Tests/src/Functional/WeightTests/RuntimeArgumentsTest.cs
+++ tests/Tests/src/Functional/WeightTests/RuntimeArgumentsTest.cs
@@ -3,6 +3,7 @@ using Armature.Core;
 using Armature.Core.Sdk;
 using FluentAssertions;
 using NUnit.Framework;
+using Tests.Util;
 
 namespace Tests.Functional.WeightTests;
 
@@ -17,9 +18,7 @@ public class RuntimeArgumentsTest
     // --arrange
     var target = CreateTarget();
 
-    target.Treat<string>().AsInstance(expectedText);
-    target.Treat<int>().AsInstance(expectedValue);
-
+    target.TreatAll().UsingArguments(expectedText, expectedValue);
     target.Treat<Subject>().AsIs();
 
     // --act
@@ -30,41 +29,57 @@ public class RuntimeArgumentsTest
     actual.Value.Should().Be(expectedValue);
   }
 
-    private static Builder CreateTarget()
-      => new(BuildStage.Cache, BuildStage.Create)
-         {
-             // inject into constructor
-             new IfFirstUnit(new IsConstructor())
+  [Test]
+  public void runtime_arguments_should_take_over_registered()
+  {
+    const string expectedText  = "expected 09765";
+    const int    expectedValue = 93979;
+
+    // --arrange
+    var target = CreateTarget();
+
+    target.TreatAll().AmendWeight(-11).UsingArguments(expectedText + "bad", expectedValue + 32);
+    target.Treat<Subject>().AsIs();
+
+    // --act
+    var actual = target.Build<Subject>(expectedText, expectedValue)!;
+
+    // --assert
+    actual.Text.Should().Be(expectedText);
+    actual.Value.Should().Be(expectedValue);
+  }
+
+  private static Builder CreateTarget()
+    => new(BuildStage.Cache, BuildStage.Create)
+       {
+           // inject into constructor
+           new IfFirstUnit(new IsConstructor())
               .UseBuildAction(
-                 new TryInOrder
-                 {
-                   new GetConstructorByInjectPointId(),       // constructor marked with [Inject] attribute has more priority
-                   new GetConstructorWithMaxParametersCount() // constructor with largest number of parameters has less priority
-                 },
-                 BuildStage.Create),
-             new IfFirstUnit(new IsParameterInfoList())
+                   new TryInOrder
+                   {
+                       new GetConstructorByInjectPointId(),       // constructor marked with [Inject] attribute has more priority
+                       new GetConstructorWithMaxParametersCount() // constructor with largest number of parameters has less priority
+                   },
+                   BuildStage.Create),
+           new IfFirstUnit(new IsParameterInfoList())
               .UseBuildAction(new BuildMethodArgumentsInDirectOrder(), BuildStage.Create),
-             new IfFirstUnit(new IsParameterInfo())
+           new IfFirstUnit(new IsParameterInfo())
               .UseBuildAction(
-                 new TryInOrder
-                 {
-                   Static.Of<BuildArgumentByParameterInjectPointId>(),
-                   Static.Of<BuildArgumentByParameterType>()
-                 },
-                 BuildStage.Create)
-         };
+                   new TryInOrder {Static.Of<BuildArgumentByParameterInjectPointId>(), Static.Of<BuildArgumentByParameterType>()},
+                   BuildStage.Create)
+       };
 
-    private class Subject
+  private class Subject
+  {
+    public const string TextParameterId = "Text";
+
+    public Subject([Inject(TextParameterId)] string text, int value)
     {
-      public const string TextParameterId = "Text";
-
-      public Subject([Inject(TextParameterId)] string text, int value)
-      {
-        Text  = text;
-        Value = value;
-      }
-
-      public int    Value { get; }
-      public string Text  { get; }
+      Text  = text;
+      Value = value;
     }
+
+    public int    Value { get; }
+    public string Text  { get; }
+  }
 }
